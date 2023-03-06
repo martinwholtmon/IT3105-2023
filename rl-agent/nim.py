17f4a65ae8710ed6510140ab729c90ae203df774
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-03-06 18:26:55 +0100

    chore: fixed typos and too long lines

--- rl-agent/nim.py
+++ rl-agent/nim.py
@@ -3,13 +3,15 @@ import random
 
 class Nim:
     """Game of Nim:
-    NIM is actually a variety of games involving pieces (or stones) on a nondescript board that players alternatively remove,
-    with the player removing the last piece being the winner, or loser, depending on the game variant.
-    Typical constraints of the game include a minimum and maximum number of pieces that can be removed at any one time,
-    a partitioning of the pieces into heaps such that players must extract from a single heap on a given turn,
-    and boards with distinct geometry that affects where pieces reside, how they can be grouped during removal, etc.
+    NIM is actually a variety of games involving pieces (or stones) on a nondescript board
+    that players alternatively remove, with the player removing the last piece being the winner,
+    or loser, depending on the game variant. Typical constraints of the game include a minimum
+    and maximum number of pieces that can be removed at any one time, a partitioning of the pieces
+    into heaps such that players must extract from a single heap on a given turn, and boards with
+    distinct geometry that affects where pieces reside, how they can be grouped during removal, etc.
 
-    To keep it simple, we do not care about grouping, just assume that all pieces are grouped togehter.
+    To keep it simple, we do not care about grouping,
+    just assume that all pieces are grouped together.
     """
 
     def __init__(self, N: int, K: int) -> None:
@@ -56,7 +58,8 @@ class Nim:
         return random.choice(self.get_legal_actions())
 
     def get_legal_actions(self) -> list[int]:
-        """Generate a list of legal actions. For this game, it would be a list of possible pieces to remove
+        """Generate a list of legal actions. For this game,
+        it would be a list of possible pieces to remove
 
         Returns:
             list[int]: Legal actions
