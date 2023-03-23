1786bcdb00536338b546e1abe643fe09b539f03a
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-03-23 10:04:55 +0100

    feat: designed the neural network (closes #11)

--- rl-agent/neural_net.py
+++ rl-agent/neural_net.py
@@ -1,27 +1,53 @@
 """Create the neural network and handles all the interactions with it."""
-import torch
 import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from helpers import is_sequence_of_type
 
 
-class ANET(torch.nn.Module):
+class ANET(nn.Module):
     def __init__(
         self,
-        n_input: int,
-        n_hidden_layers: int,
-        n_output: int,
-        n_units: int,
+        input_shape: tuple[int, int],
+        hidden_units: list[int],
+        output_shape: tuple[int, int],
         activation_function: str,
     ) -> None:
         """Init a ANET
 
         Args:
-            n_input (int): Number of max input params/features (largest game space)
-            n_hidden_layers (int): Number of hidden layers
-            n_output (int): Number of max output params (largest action space)
-            n_units (int): Number of units (neurons) per layer
+            input_shape (tuple[int, int]): Shape of input params/features
+            hidden_units (list[int]): Number of units (neurons) per layer
+            output_shape (tuple[int, int]): Shape of max output params (largest action space)
             activation_function (str): Activation function to use: linear, sigmoid, yanh, RELU
         """
-        pass
+        # Inherit from nn.Module
+        super(ANET, self).__init__()
+
+        # Check params
+        is_sequence_of_type("input_shape", input_shape, tuple, int, min=1, max=2)
+        is_sequence_of_type("hidden_units", hidden_units, list, int, min=1)
+        is_sequence_of_type("output_shape", output_shape, tuple, int, min=1, max=2)
+
+        # Set params
+        self.activation_function = activation_function
+
+        # Define input layer
+        self.input_layer = nn.Linear(np.multiply.reduce(input_shape), hidden_units[0])
+
+        # Define hidden layers
+        self.hidden_layers = nn.ModuleList()
+        if len(hidden_units) == 1:
+            self.hidden_layers.append(nn.Linear(hidden_units[0], hidden_units[0]))
+        else:
+            for i, j in zip(hidden_units, hidden_units[1:]):
+                self.hidden_layers.append(nn.Linear(i, j))
+
+        # Define output layer
+        self.output_layer = nn.Linear(
+            hidden_units[-1], np.multiply.reduce(output_shape)
+        )
 
     def predict(self, state) -> any:
         raise NotImplementedError
