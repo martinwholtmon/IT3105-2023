29469ff01cb003ab24884f794d4f11bdd753b218
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-03-24 22:50:23 +0100

    refactored param hidden_units to hidden_layers. Added functionality to convert state to tensor and tensor to np array.

--- rl-agent/neural_net.py
+++ rl-agent/neural_net.py
@@ -3,30 +3,31 @@ import numpy as np
 import torch
 import torch.nn as nn
 from helpers import is_sequence_of_type
+from state_manager import State
 
 
 class ANET(nn.Module):
     def __init__(
         self,
         input_shape: tuple[int, int],
-        hidden_units: list[int],
+        hidden_layers: list[int],
         output_shape: tuple[int, int],
         activation_function: str,
     ) -> None:
-        """Init a ANET
+        """Init the neural network.
 
         Args:
             input_shape (tuple[int, int]): Shape of input params/features
-            hidden_units (list[int]): Number of units (neurons) per layer
+            hidden_layers (list[int]): Number of units (neurons) per layer
             output_shape (tuple[int, int]): Shape of max output params (largest action space)
-            activation_function (str): Activation function to use: linear, sigmoid, tanh, RELU
+            activation_function (str): Activation function to use: linear, sigmoid, tanh, relu
         """
         # Inherit from nn.Module
         super(ANET, self).__init__()
 
         # Check params
         is_sequence_of_type("input_shape", input_shape, tuple, int, min=1, max=2)
-        is_sequence_of_type("hidden_units", hidden_units, list, int, min=1)
+        is_sequence_of_type("hidden_units", hidden_layers, list, int, min=1)
         is_sequence_of_type("output_shape", output_shape, tuple, int, min=1, max=2)
 
         # Set params
@@ -36,48 +37,74 @@ class ANET(nn.Module):
         modules = []
 
         # input layer
-        modules.append(nn.Linear(np.multiply.reduce(input_shape), hidden_units[0]))
+        modules.append(nn.Linear(np.multiply.reduce(input_shape), hidden_layers[0]))
 
         # hidden layers
-        if len(hidden_units) == 1:
-            modules.append(nn.Linear(hidden_units[0], hidden_units[0]))
+        if len(hidden_layers) == 1:
+            modules.append(nn.Linear(hidden_layers[0], hidden_layers[0]))
             if self.activation_function is not None:
                 modules.append(self.activation_function)
         else:
-            for i, j in zip(hidden_units, hidden_units[1:]):
+            for i, j in zip(hidden_layers, hidden_layers[1:]):
                 modules.append(nn.Linear(i, j))
                 if self.activation_function is not None:
                     modules.append(self.activation_function)
 
         # output layer
-        modules.append(nn.Linear(hidden_units[-1], np.multiply.reduce(output_shape)))
+        modules.append(nn.Linear(hidden_layers[-1], np.multiply.reduce(output_shape)))
         modules.append(nn.Softmax())
 
         # Define the model
         self.layers = nn.Sequential(*modules)
 
-    def predict(self, state) -> any:
+        # Define the loss function
+        self.loss_function = nn.CrossEntropyLoss()
+
+    def predict(self, state: State) -> any:
+        prediction = self.forward(state.get_state())
+
+    def forward(self, x: np.ndarray):
+        """Do a forward pass in the network to predict a state
+
+        Args:
+            x (np.ndarray): game state
+
+        Returns:
+            np.ndarray: predicted values as a 1d np.ndarray
+        """
+        return tensor_to_np(self.layers(np_to_tensor(x)))
+
+    def update(self, batch):
         raise NotImplementedError
 
-    def forward(self, x):
-        return scale_output(self.layers(state_to_tensor(x)))
 
-    def update(self):
-        raise NotImplementedError
-
-
-def state_to_tensor(state) -> torch.Tensor:
+def np_to_tensor(x: State) -> torch.Tensor:
     """Converts a state to a tensor.
     Flatten the tensor - in case of ndarray of 2+d
     Convert it to floats: PyTorch parameters expect float32 input
 
+    Args:
+        state (np.ndarray): game state
+
     Returns:
         torch.Tensor: game state as a tensor
     """
-    return torch.from_numpy(state).flatten().float()
+    return torch.from_numpy(x).flatten().float()
 
 
-def scale_output(x) -> np.ndarray:
+def tensor_to_np(tensor: torch.Tensor) -> np.ndarray:
+    """Convert a tensor to a numpy representation
+
+    Args:
+        tensor (torch.Tensor): _description_
+
+    Returns:
+        np.ndarray: _description_
+    """
+    return tensor.detach().numpy()
+
+
+def scale_prediction(x) -> np.ndarray:
     """The output layer must have a fixed number of outputs
     even though there might not be so many avaiable actions.
     From any given state, there is a reduced number of legal actions.
